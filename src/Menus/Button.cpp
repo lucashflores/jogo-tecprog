33633ef2dd087b6bb57d6daf64fe80a95fb8b053
JP <joaopiams@gmail.com>
2021-11-26 16:05:23 -0300

    Made visual changes

--- src/Menus/Button.cpp
+++ src/Menus/Button.cpp
@@ -14,7 +14,14 @@ Button::Button(Coordinates::Vector<float> pos, std::string t): Ent(Id::button), 
     text = new sf::Text();
     text->setString(t);
     text->setFont(*font);
-    text->setPosition(pos.getX() - 10.f, pos.getY());
+    text->setCharacterSize(25);
+
+    //center text
+    sf::FloatRect textRect = text->getLocalBounds();
+    text->setOrigin(textRect.left + textRect.width/2.0f,textRect.top  + textRect.height/2.0f);
+    text->setPosition(640.f,pos.getY()-15.f);
+
+    //text->setPosition(pos.getX() - 105.f, pos.getY());
     text->setFillColor(sf::Color::White);
 }
 Button::~Button() {
@@ -50,7 +57,7 @@ void Button::render() {
 
 void Button::update() {
     if (getSelected())
-        text->setFillColor(sf::Color::Red);
+        text->setFillColor(sf::Color(175, 175, 189, 255));
     else
-        text->setFillColor(sf::Color::White);
+        text->setFillColor(sf::Color(22, 22, 28, 255));
 }
